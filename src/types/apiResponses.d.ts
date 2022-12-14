44a66bd19d3714dd909b42a6755bb0c3da28b2a7
hayapo <keep.forward1001@gmail.com>
2022-12-15 07:54:26 +0900

    [feature] 選択済み都道府県の型を追加

--- src/types/apiResponses.d.ts
+++ src/types/apiResponses.d.ts
@@ -1,13 +1,21 @@
+// RESAS APIから帰ってくる都道府県情報の型
 export type Prefecture = {
   prefCode: number
   prefName: string
 }
 
+// 都道府県取得(API/api/v1/prefectures)のレスポンスの型
 export type PrefectureResponse = {
   message: string | null
   result: Prefecture[]
 }
 
+// チェックボックスで選択した都道府県の型
+export type SelectPrefecture = Prefecture & {
+  checked: boolean
+}
+
+// RESAS APIから帰ってくる人口構成情報の型
 export type Population = {
   label: string
   data: {
@@ -17,6 +25,7 @@ export type Population = {
   }[]
 }
 
+// 都道府県別人口構成取得(API/api/v1/population)のレスポンスの型
 export type PopulationResponse = {
   message: string | null
   result: {
