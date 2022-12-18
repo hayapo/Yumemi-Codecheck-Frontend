f8c1cc8cc9ce9806c51eeb9a0f1e307c840bf443
hayapo <keep.forward1001@gmail.com>
2022-12-19 06:21:04 +0900

    [chore] 未使用ファイルの削除

--- src/lib/hooks/usePrefecture.ts
+++
@@ -1,7 +0,0 @@
-import { getPrefectures } from "lib/api/getPrefectures"
-import useSWR from "swr"
-
-export const usePrefecture = () => {
-  const { data } = useSWR("/api/v1/prefectures", getPrefectures)
-  return data
-}
